1ad312be47473294cd89c859a7307564c9c49c88
kuldip-zignuts <kuldipp@zignuts.com>
2023-03-27 11:22:57 +0530

    get id from params

--- pages/products/[productid].js
+++ pages/products/[productid].js
@@ -1,7 +1,11 @@
+import { useRouter } from "next/router"
+
 const Product_Id = ()=>{
+    const router = useRouter();
+    const prodctid = router.query.productid
     return(
         <>
-        <h1>You Are Into Individual Products</h1>
+        <h1>You Are Into Individual Products {prodctid}</h1>
         </>
     )
 }
