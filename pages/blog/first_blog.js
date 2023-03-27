70bed45e04f446f8d48f2481cff96ac00ed46f52
kuldip-zignuts <kuldipp@zignuts.com>
2023-03-27 10:59:38 +0530

    nested route

---
+++ pages/blog/first_blog.js
@@ -0,0 +1,8 @@
+const First_blog = ()=>{
+    return(
+        <div>
+            <h1>First Blog</h1>
+        </div>
+    )
+}
+export default First_blog
---
+++ pages/blog/second_blog.js
@@ -0,0 +1,8 @@
+const Second_blog = ()=>{
+    return(
+        <div>
+            <h1>Second Blog</h1>
+        </div>
+    )
+}
+export default Second_blog
